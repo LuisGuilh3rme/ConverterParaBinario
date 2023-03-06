292bf10b7baf28ee358d5889d1a61af3d15f2099
Luis Guilherme <luislebert01@outlook.com>
2023-03-06 13:55:41 -0300

    Add project files.

---
+++ ConverterParaBinario/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Informe o número a ser convertido para binário:");
+int numero = int.Parse(Console.ReadLine());
+
+int[] resposta = converterBinario(numero, new int[8]);
+
+foreach (int digito in resposta)
+{
+    Console.Write(digito);
+}
+int[] converterBinario(int numero, int[] aux, int i = 0)
+{
+    if (numero == 0) return aux;
+    aux[7 - i] = (numero % 2);
+    return converterBinario((numero / 2), aux, ++i);
+}
